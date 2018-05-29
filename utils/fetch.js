515f9176c66f2166f2b119907e5b0c374644f31a
unknown <linhzh@xmmy.com>
2018-05-29 15:25:10 +0800

    添加we-ui

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,8 @@
 /**
  * 远程api调用工具
- * @param api 请求访问url
+ * @param api 请求url
+ * @param path 路径
+ * @param method 请求方法 [GET POST PUT DELETE OPTION]
  */
 module.exports = function (api, path, params, method) {
   let token = wx.getStorageSync('token') || '';
